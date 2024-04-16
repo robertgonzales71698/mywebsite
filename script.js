b92d5b186697aadf6d3f2237a92aacf7fb817eff
Robert Gonzales <robert.m.gonzales-1@ou.edu>
2024-04-15 22:54:19 -0700

    Update the marquee

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // You can add JavaScript functionalities if needed
 
 // Repeat the content of the marquee with more space between each repetition
-const marqueeContent = "Still in development. "; // Content to repeat
+const marqueeContent = "Still in Development. "; // Content to repeat
 const repetitions = 10; // Number of repetitions
-const extraSpaces = " ".repeat(10); // Add more spaces between repetitions
+const extraSpaces = "         ".repeat(10); // Add more spaces between repetitions
 const spacedContent = (marqueeContent + extraSpaces).repeat(repetitions); // Repeat content and add more spaces between each repetition
 document.getElementById("repeat-marquee").innerText = spacedContent;
